4673d3b233e39408774076eec87f0c18d7a89830
tongjiyiming <tongjiyiming@gmail.com>
2018-01-28 22:00:59 -0500

    pass grading version

--- Prinston_Algorithm/src/Percolation.java
+++ Prinston_Algorithm/src/Percolation.java
@@ -1,254 +1,322 @@
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
- * Percolation assignment
- * It is modeled as a grid board which material run into from the first row, and run out from the last row
- * @since 01.27.2018
+ * Percolation assignment It is modeled as a grid board which material run into
+ * from the first row, and run out from the last row 01.27.2018
+ * 
  * @author liming
  *
  */
 public class Percolation {
+    private final int gridSize; // grid size for the physics problem.
+    private final WeightedQuickUnionUF uf; // the WeightedQuickUnionUF class instance
+    private final int start = 0; // a pseudo node connected to all cells in first row
+    private final int end; // a pseudo node connected to all cells in last row
+    private boolean[] cellState; // a boolean array to record the open/close state of all the cells
+    private int numberOfOpenCell = 0; // value to record how many cell is open
+
     /**
-     * grid size for the physics problem
-     */
-    public int gridSize;
-    /**
-     * the WeightedQuickUnionUF class instance to model and compute the connectivity
-     */
-    private WeightedQuickUnionUF UF;
-    /**
-     * a pseudo node connected to all cells in first row
-     */
-    private int start=0;
-    /**
-     * a pseudo node connected to all cells in last row
-     */
-    private int end;
-    /**
-     * a boolean array to record the open/close state of all the cells
-     */
-    private boolean[] cellState;
-    /**
-     * value to record how many cell is open
-     */
-    private int numberOfOpenCell=0;
-    
-    /**
-     * constructor method to build a grid with all cell close
-     * @param n size of grid
+     * constructor method to build a grid with all cell close.
+     * 
+     * @param n
+     *            size of grid
+     * 
      */
     public Percolation(int n) {
-	// create n-by-n grid, with all sites blocked
-	if (n < 1) throw new java.lang.IllegalArgumentException("size of grid should be larger than 0!");
-	gridSize = n;
-	end = gridSize*gridSize + 1;
-	cellState = new boolean[end+1];
-	
-	// initially, connect start cell with first row, and end cell with last row
-	UF = new WeightedQuickUnionUF(end+1);
-	for (int j = 1; j <= gridSize; j++) {
-		UF.union(start, j);
-		UF.union(end, (gridSize-1)*gridSize+j);
-	}
+    // create n-by-n grid, with all sites blocked
+    if (n < 1)
+        throw new java.lang.IllegalArgumentException("size of grid should be larger than 0!");
+    gridSize = n;
+    end = gridSize * gridSize + 1;
+    cellState = new boolean[end + 1];
+
+    // initially, connect start cell with first row, and end cell with last row
+    uf = new WeightedQuickUnionUF(end + 1);
+    for (int j = 1; j <= gridSize; j++) {
+        uf.union(start, j);
+        uf.union(end, (gridSize - 1) * gridSize + j);
     }
-    
-    public void open(int row, int col) {
-	// open site (row, col) if it is not open already
-    		if (row < 1 || row > gridSize || col < 1 || col > gridSize) throw new java.lang.IllegalArgumentException(
-    				"size of grid should be in range [0, gridSize]!");
-    		
-    		int index = this.getIndex(row, col);
-    		if ( !cellState[index] ) {
-    			cellState[index] = true;
-    			numberOfOpenCell ++;
-    			modifyUF(row, col);
-    		}
     }
-    
+
     /**
-     * with a new open cell (row, col), modify the Union-Find data model
+     * open a site based on its (row, col) index.
+     * 
      * @param row
+     *            row index
      * @param col
+     *            column index
+     */
+    public void open(int row, int col) {
+    // open site (row, col) if it is not open already
+    if (row < 1 || row > gridSize || col < 1 || col > gridSize)
+        throw new java.lang.IllegalArgumentException("size of grid should be in range [0, gridSize]!");
+
+    int index = this.getIndex(row, col);
+    if (!cellState[index]) {
+        cellState[index] = true;
+        numberOfOpenCell++;
+        modifyUF(row, col);
+    }
+    }
+
+    /**
+     * with a new open cell (row, col), modify the Union-Find data model.
+     * 
+     * @param row
+     *            row index
+     * @param col
+     *            column index
      */
     private void modifyUF(int row, int col) {
-    		int index = this.getIndex(row, col);
-    		
-    		// four corner cases
-		if (row == 1 && col == 1) {
-//			StdOut.println("up left corner case");
-			this.checkOpenAndUnion(index, this.getIndex(2, 1));
-		}
-		else if ( row == 1 && col == gridSize) {
-//			StdOut.println("up right corner case");
-			this.checkOpenAndUnion(index, this.getIndex(2, col));
-		}
-		else if ( row == gridSize && col == 1 ) {
-//			StdOut.println("down left corner case");
-			this.checkOpenAndUnion(index, this.getIndex(row-1, 1));
-		}
-		else if ( row == gridSize && col == gridSize ) {
-//			StdOut.println("down right corner case");
-			this.checkOpenAndUnion(index, this.getIndex(row-1, col));
-		}
-		// four edge cases
-		else if ( row != 1 && row != gridSize && col == 1) {
-//			StdOut.println("left edge case");
-			this.checkOpenAndUnion(index, this.getIndex(row-1, col));
-			this.checkOpenAndUnion(index, this.getIndex(row+1, col));
-			this.checkOpenAndUnion(index, this.getIndex(row, col+1));
-		}
-		else if ( row != 1 && row != gridSize && col == gridSize ) {
-//			StdOut.println("right edge case");
-			this.checkOpenAndUnion(index, this.getIndex(row-1, col));
-			this.checkOpenAndUnion(index, this.getIndex(row+1, col));
-			this.checkOpenAndUnion(index, this.getIndex(row, col-1));
-		}
-		else if ( row == 1 && col != 1 && col != gridSize ) {
-//			StdOut.println("up edge case");
-			this.checkOpenAndUnion(index, this.getIndex(row+1, col));
-		}
-		else if ( row == gridSize && col != 1 && col != gridSize ) {
-//			StdOut.println("down edge case");
-			this.checkOpenAndUnion(index, this.getIndex(row-1, col));
-		}
-		else {
-//			StdOut.println("normal case");
-			this.checkOpenAndUnion(index, this.getIndex(row-1, col));
-			this.checkOpenAndUnion(index, this.getIndex(row+1, col));
-			this.checkOpenAndUnion(index, this.getIndex(row, col-1));
-			this.checkOpenAndUnion(index, this.getIndex(row, col+1));
-		}
-	}
+    int index = this.getIndex(row, col);
+
+    // four corner cases
+    if (row == 1 && col == 1) {
+        // StdOut.println("up left corner case");
+        this.checkOpenAndUnion(index, this.getIndex(2, 1));
+    } else if (row == 1 && col == gridSize) {
+        // StdOut.println("up right corner case");
+        this.checkOpenAndUnion(index, this.getIndex(2, col));
+    } else if (row == gridSize && col == 1) {
+        // StdOut.println("down left corner case");
+        this.checkOpenAndUnion(index, this.getIndex(row - 1, 1));
+    } else if (row == gridSize && col == gridSize) {
+        // StdOut.println("down right corner case");
+        this.checkOpenAndUnion(index, this.getIndex(row - 1, col));
+    }
+    // four edge cases
+    else if (row != 1 && row != gridSize && col == 1) {
+        // StdOut.println("left edge case");
+        this.checkOpenAndUnion(index, this.getIndex(row - 1, col));
+        this.checkOpenAndUnion(index, this.getIndex(row + 1, col));
+        this.checkOpenAndUnion(index, this.getIndex(row, col + 1));
+    } else if (row != 1 && row != gridSize && col == gridSize) {
+        // StdOut.println("right edge case");
+        this.checkOpenAndUnion(index, this.getIndex(row - 1, col));
+        this.checkOpenAndUnion(index, this.getIndex(row + 1, col));
+        this.checkOpenAndUnion(index, this.getIndex(row, col - 1));
+    } else if (row == 1 && col != 1 && col != gridSize) {
+        // StdOut.println("up edge case");
+        this.checkOpenAndUnion(index, this.getIndex(row + 1, col));
+    } else if (row == gridSize && col != 1 && col != gridSize) {
+        // StdOut.println("down edge case");
+        this.checkOpenAndUnion(index, this.getIndex(row - 1, col));
+    } else {
+        // StdOut.println("normal case");
+        this.checkOpenAndUnion(index, this.getIndex(row - 1, col));
+        this.checkOpenAndUnion(index, this.getIndex(row + 1, col));
+        this.checkOpenAndUnion(index, this.getIndex(row, col - 1));
+        this.checkOpenAndUnion(index, this.getIndex(row, col + 1));
+    }
+    }
 
     /**
-     * check if a cell is open or not
+     * check if a cell is open or not.
+     * 
      * @param row
+     *            row index
      * @param col
+     *            col index
      * @return true if cell is open, false otherwise
      */
-	public boolean isOpen(int row, int col) {
-	// is site (row, col) open?
-		if (row < 1 || row > gridSize || col < 1 || col > gridSize) throw new java.lang.IllegalArgumentException("size of grid should be larger than 0!");		
-    		return cellState[this.getIndex(row, col)];
-	
-    }
-	
-    public boolean isFull(int row, int col) {
-	// is site (row, col) full?
-		if (row < 1 || row > gridSize || col < 1 || col > gridSize) throw new java.lang.IllegalArgumentException("size of grid should be larger than 0!");
+    public boolean isOpen(int row, int col) {
+    // is site (row, col) open?
+    if (row < 1 || row > gridSize || col < 1 || col > gridSize)
+        throw new java.lang.IllegalArgumentException("size of grid should be larger than 0!");
+    return cellState[this.getIndex(row, col)];
 
-    		return UF.find(1) == UF.find(this.getIndex(row, col));
     }
-    
+
     /**
-     * require API; return number of open cells
+     * check if a site is connected with top row.
+     * 
+     * @param row
+     *            row index
+     * @param col
+     *            column index
+     * @return
+     */
+    public boolean isFull(int row, int col) {
+    // is site (row, col) full?
+    if (row < 1 || row > gridSize || col < 1 || col > gridSize)
+        throw new java.lang.IllegalArgumentException("size of grid should be larger than 0!");
+
+    return isOpen(row, col) && uf.find(1) == uf.find(this.getIndex(row, col));
+    }
+
+    /**
+     * return number of open cells.
+     * 
      * @return
      */
     public int numberOfOpenSites() {
-	// number of open sites
-    		return numberOfOpenCell;
-    }
-    
-    public boolean percolates() {
-	// does the system percolate?
-    		return UF.find(start) == UF.find(end);
+    // number of open sites
+    return numberOfOpenCell;
     }
 
+    /**
+     * check if grid board percolates.
+     * 
+     * @return
+     */
+    public boolean percolates() {
+    // does the system percolate?
+    if (gridSize == 1) {
+        return isOpen(1, 1);
+    } else {
+        return uf.find(start) == uf.find(end);
+    }
+    }
+
+    /**
+     * print out the state of grid.
+     */
     private void printGrid() {
-    		StdOut.println("total length of UF: " + cellState.length);
-    		for (int r = 1; r <= gridSize; r++) {
-    			String[] str = new String[gridSize];
-    			for ( int c = 1; c <= gridSize; c++) {
-    				if ( this.isOpen(r, c) ) {
-    					str[c-1] = String.format("<(%s, %s)>", r, c);
-    				}
-    				else {
-    					str[c-1] = String.format(" (%s, %s) ", r, c);
-    				}
-    			}
-    			StdOut.println(String.join(" ", str));
-    		}
+    StdOut.println("total length of uf: " + cellState.length);
+    for (int r = 1; r <= gridSize; r++) {
+        String[] str = new String[gridSize];
+        for (int c = 1; c <= gridSize; c++) {
+        if (this.isOpen(r, c)) {
+            str[c - 1] = String.format("<(%s, %s)>", r, c);
+        } else {
+            str[c - 1] = String.format(" (%s, %s) ", r, c);
+        }
+        }
+        StdOut.println(String.join(" ", str));
     }
-    
+    }
+
+    /**
+     * check if two sites are connected.
+     * 
+     * @param row1
+     *            row index of first site
+     * @param col1
+     *            column index of second site
+     * @param row2
+     *            row index of first site
+     * @param col2
+     *            column index of second site
+     * 
+     * @return
+     */
     private boolean isConnected(int row1, int col1, int row2, int col2) {
-    		int ind1 = getIndex(row1, col1);
-    		int ind2 = getIndex(row2, col2);
-    		return UF.connected(ind1, ind2);
+    int ind1 = getIndex(row1, col1);
+    int ind2 = getIndex(row2, col2);
+    return uf.connected(ind1, ind2);
     }
-    
+
+    /**
+     * an utility function to compute the Union-Find index from (row, col).
+     * 
+     * @param row
+     *            row index
+     * @param col
+     *            col index
+     * 
+     * @return
+     */
     private int getIndex(int row, int col) {
-    		return gridSize * (row - 1) + col;
+    return gridSize * (row - 1) + col;
     }
-    
+
+    /**
+     * check if its neighbor is open, if yes, union with neighbor.
+     * 
+     * @param s
+     *            checked site itself
+     * @param neighbor
+     *            neighbor of checked site
+     */
     private void checkOpenAndUnion(int s, int neighbor) {
-		if ( cellState[neighbor] ) {
-			UF.union(s, neighbor);
-		}
+    if (cellState[neighbor]) {
+        uf.union(s, neighbor);
     }
-    
+    }
+
     public static void main(String[] args) {
-	// test client (optional)
-    	Percolation P = new Percolation(4);
-    	StdOut.println("grid size:" + P.gridSize);
-    	StdOut.println(P.UF.find(2));
-    	StdOut.println(P.UF.find(8));
-    	StdOut.println(P.UF.find(14));
-    	StdOut.println("(0, 3) connected? " + P.UF.connected(0, 3));
-    	StdOut.println("(1, 1) (1, 2) connected? " + P.isConnected(1, 1, 1, 2));
-    	StdOut.println("(17, 3) connected? " + P.UF.connected(17, 3));
-    	StdOut.println("(17, 15) connected? " + P.UF.connected(17, 15));
-    	P.printGrid();
-    	// test corner cases
-    	StdOut.println("open site (1, 2), (1, 1)>>>>>>");
-    	P.open(1, 2);
-    	StdOut.println(P.UF.find(16));
-    	P.open(1, 1);
-    	StdOut.println(P.UF.find(16));
-    	P.printGrid();
-    	StdOut.println("is (1, 2) connected to (1, 1)?" + P.isConnected(1, 2, 1, 1));
-    	StdOut.println("open site (2, 1)>>>>>>");
-    	P.open(2, 1);
-    	StdOut.println(P.UF.find(16));
-    	P.printGrid();
-    	StdOut.println("is (1, 1) (2, 1) connected? " + P.isConnected(1, 1, 2, 1));
-    	StdOut.println("is (1, 2) (2, 1) connected? " + P.isConnected(1, 2, 2, 1));
-    	StdOut.println("open site (4, 1)>>>>>>");
-    P.open(4, 1);
-    StdOut.println(P.UF.find(16));
-    P.printGrid();
-    StdOut.println("is (4, 1) (3, 1) connected? " + P.isConnected(4, 1, 3, 1));
-    P.open(4,  2);
-    StdOut.println(P.UF.find(16));
-    P.printGrid();
-    StdOut.println("is (3, 1) (4, 2) connected? " + P.isConnected(3, 1, 4, 2));
+    // test client (optional)
+    Percolation perc = new Percolation(4);
+    StdOut.println("grid size:" + perc.gridSize);
+    StdOut.println(perc.uf.find(2));
+    StdOut.println(perc.uf.find(8));
+    StdOut.println(perc.uf.find(14));
+    StdOut.println("(0, 3) connected? " + perc.uf.connected(0, 3));
+    StdOut.println("(1, 1) (1, 2) connected? " + perc.isConnected(1, 1, 1, 2));
+    StdOut.println("(17, 3) connected? " + perc.uf.connected(17, 3));
+    StdOut.println("(17, 15) connected? " + perc.uf.connected(17, 15));
+    perc.printGrid();
+    // test corner cases
+    StdOut.println("open site (1, 2), (1, 1)>>>>>>");
+    perc.open(1, 2);
+    StdOut.println(perc.uf.find(16));
+    perc.open(1, 1);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (1, 2) connected to (1, 1)?" + perc.isConnected(1, 2, 1, 1));
+    StdOut.println("open site (2, 1)>>>>>>");
+    perc.open(2, 1);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (1, 1) (2, 1) connected? " + perc.isConnected(1, 1, 2, 1));
+    StdOut.println("is (1, 2) (2, 1) connected? " + perc.isConnected(1, 2, 2, 1));
+    StdOut.println("open site (4, 1)>>>>>>");
+    perc.open(4, 1);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (4, 1) (3, 1) connected? " + perc.isConnected(4, 1, 3, 1));
+    perc.open(4, 2);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (3, 1) (4, 2) connected? " + perc.isConnected(3, 1, 4, 2));
     StdOut.println("Open site (2, 4)>>>>>>");
-    P.open(2,  4);
-    StdOut.println(P.UF.find(16));
-    P.printGrid();
-    StdOut.println("is (1, 4) (2, 4) connected? " + P.isConnected(1, 4, 2, 4));
-    P.open(1, 4);
-    StdOut.println(P.UF.find(16));
-    P.printGrid();
-    StdOut.println("is (1, 4) (2, 4) connected? " + P.isConnected(1, 4, 2, 4));
-    P.open(3, 4);
-    StdOut.println(P.UF.find(16));
-    P.printGrid();
-    StdOut.println("is (3, 4) (4, 4) connected? " + P.isConnected(3, 4, 4, 4));
-    P.open(4, 3);
-    StdOut.println("is (4, 3) (3, 4) connnected? " + P.isConnected(4, 3, 3, 4));
-    P.printGrid();
-    StdOut.println("number of open cell: " + P.numberOfOpenSites());
-    StdOut.println("is (4, 4) full? " + P.isFull(4, 4));
-    StdOut.println("is (3, 4) full? " + P.isFull(3, 4));
-    StdOut.println("is (2, 1) full? " + P.isFull(2, 1));
-    StdOut.println("is (4, 1) full? " + P.isFull(4, 1));
-    StdOut.println("is percolate? " + P.percolates());
-    P.open(3, 2);
-    P.printGrid();
-    StdOut.println("is percolate? " + P.percolates());
-    P.open(3, 3);
-    P.printGrid();
-    StdOut.println("is percolate? " + P.percolates());
+    perc.open(2, 4);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (1, 4) (2, 4) connected? " + perc.isConnected(1, 4, 2, 4));
+    perc.open(1, 4);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (1, 4) (2, 4) connected? " + perc.isConnected(1, 4, 2, 4));
+    perc.open(3, 4);
+    StdOut.println(perc.uf.find(16));
+    perc.printGrid();
+    StdOut.println("is (3, 4) (4, 4) connected? " + perc.isConnected(3, 4, 4, 4));
+    perc.open(4, 3);
+    StdOut.println("is (4, 3) (3, 4) connnected? " + perc.isConnected(4, 3, 3, 4));
+    perc.printGrid();
+    StdOut.println("number of open cell: " + perc.numberOfOpenSites());
+    StdOut.println("is (4, 4) full? " + perc.isFull(4, 4));
+    StdOut.println("is (3, 4) full? " + perc.isFull(3, 4));
+    StdOut.println("is (2, 1) full? " + perc.isFull(2, 1));
+    StdOut.println("is (4, 1) full? " + perc.isFull(4, 1));
+    StdOut.println("is percolate? " + perc.percolates());
+    perc.open(3, 2);
+    perc.printGrid();
+    StdOut.println("is percolate? " + perc.percolates());
+    perc.open(3, 3);
+    perc.printGrid();
+    StdOut.println("is percolate? " + perc.percolates());
+    // corner case 1
+    perc = new Percolation(1);
+    StdOut.println("grid size:" + perc.gridSize);
+    StdOut.println(perc.uf.find(1));
+    StdOut.println(perc.percolates());
+    perc.open(1, 1);
+    perc.printGrid();
+    StdOut.println("is percolate? " + perc.percolates());
+    // corner case 2
+    perc = new Percolation(2);
+    StdOut.println("grid size:" + perc.gridSize);
+    StdOut.println(perc.isOpen(1, 1));
+    StdOut.println(perc.isFull(1, 1));
+    perc.printGrid();
+    StdOut.println("(1, 1) (2, 2) connected? " + perc.isConnected(1, 1, 2, 2));
+    StdOut.println(perc.percolates());
+    perc.open(1, 1);
+    perc.printGrid();
+    StdOut.println("(1, 1) open? " + perc.isOpen(1, 1));
+    StdOut.println("(1, 1) full? " + perc.isFull(1, 1));
     }
- }
+}
--- Prinston_Algorithm/src/PercolationStats.java
+++ Prinston_Algorithm/src/PercolationStats.java
@@ -3,68 +3,122 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
 import java.lang.Math;
 
+/**
+ * Assignment 1 This PercolationStats class runs Monte Carlo Simulation with a
+ * specific grid size of site board, and number of trials. It uses Union-Find as
+ * its underlining data structure and to compute connectivity. It uses weighted
+ * Union-Find with Pave improvement algorithm. use command-line: java
+ * PercolationStats 100 1000 to execute a board of grid size 100, and run 1000
+ * simulation. 01.28.2018
+ * 
+ * @author liming
+ *
+ */
 public class PercolationStats {
-	private double mean;
-	private double stddev;
-	private double confidenceLo;
-	private double confidenceHi;
-	
+    private final double mean;
+    private final double stddev; // standard deviation value
+    private final double confidenceLo; // the lower side of confidence interval
+    private final double confidenceHi; // the higher side of confidence interval
+
+    /**
+     * constructor. It runs the Monte Carlo simulation and compute statistics.
+     * 
+     * @param n
+     *            grid size
+     * @param trials
+     *            number of simulations
+     */
     public PercolationStats(int n, int trials) {
-	    // perform trials independent experiments on an n-by-n grid
-    		double[] threshold = new double[trials];
-	    for (int t = 1; t <= trials; t++) {
-	    		double thres = runPercolation(n);
-	 	    threshold[t-1] = thres;
-	    }
-	    mean = StdStats.mean(threshold);
-	    stddev = StdStats.stddev(threshold);
-	    confidenceLo = mean - 1.96 * stddev / Math.sqrt(trials);
-	    confidenceHi = mean + 1.96 * stddev / Math.sqrt(trials);
+        if ( n < 1 || trials < 1)
+            throw new java.lang.IllegalArgumentException("size of grid or trials number should be larger than 0!");
+        
+    // perform trials independent experiments on an n-by-n grid
+        double[] threshold = new double[trials];
+        for (int t = 1; t <= trials; t++) {
+            double thres = runPercolation(n);
+            threshold[t - 1] = thres;
+        }
+        mean = StdStats.mean(threshold);
+        stddev = StdStats.stddev(threshold);
+        confidenceLo = mean - 1.96 * stddev / Math.sqrt(trials);
+        confidenceHi = mean + 1.96 * stddev / Math.sqrt(trials);
     }
 
+    /**
+     * one simulation run to uniformly open a site until percolate, compute the
+     * threshold of portion of open sites.
+     * 
+     * @param n
+     *            grid size
+     * @return
+     */
     private double runPercolation(int n) {
-		// run one percolation simulation
-    		Percolation P = new Percolation(n);
-    		while ( !P.percolates() ) {
-    			int row = StdRandom.uniform(n) + 1;
-    			int col = StdRandom.uniform(n) + 1;
-    			if ( !P.isOpen(row, col) ) {
-    				P.open(row, col);
-    			}
-    			if ( P.percolates() ) break;
-    		}
-    		double num = P.numberOfOpenSites();
-		return num * 1.0 / n / n;
-	}
-    
-	public double mean() {
-		// sample mean of percolation threshold
-		return mean;
-	}
-	public double stddev() {
-		// sample standard deviation of percolation threshold
-		return stddev;
-	}
-	public double confidenceLo() {
-		// low  endpoint of 95% confidence interval
-		return confidenceLo;
-	}
-	public double confidenceHi() {
-		// high endpoint of 95% confidence interval
-		return confidenceHi;
-	}
+    // run one percolation simulation
+        Percolation P = new Percolation(n);
+        while (true) {
+            int row = StdRandom.uniform(n) + 1;
+            int col = StdRandom.uniform(n) + 1;
+            if (!P.isOpen(row, col)) {
+                P.open(row, col);
+            }
+            if (row == n && P.isFull(row, col)) {
+                break;
+            }
+        }
+        double num = P.numberOfOpenSites();
+        return num * 1.0 / n / n;
+    }
 
-	public static void main(String[] args) {
-		// test client (described below)
-		int N = Integer.parseInt(args[0]);
-		int trials = Integer.parseInt(args[1]);
-		StdOut.println("input of grid size: " + N);
-		StdOut.println("input of trials number: " + trials);
-		PercolationStats P = new PercolationStats(N, trials);
-		StdOut.println("mean: " + P.mean());
-		StdOut.println("stddev: " + P.stddev());
-		StdOut.println("confidenceLo: " + P.confidenceLo());
-		StdOut.println("confidenceHi: " + P.confidenceHi());
+    /**
+     * return mean value.
+     * 
+     * @return
+     */
+    public double mean() {
+    // sample mean of percolation threshold
+	return mean;
+    }
 
-	}
+    /**
+     * return standard deviation value.
+     * 
+     * @return
+     */
+    public double stddev() {
+    // sample standard deviation of percolation threshold
+	return stddev;
+    }
+
+    /**
+     * return lower side of confidence interval.
+     * 
+     * @return
+     */
+    public double confidenceLo() {
+    // low endpoint of 95% confidence interval
+	return confidenceLo;
+    }
+
+    /**
+     * return higher side of confidence interval.
+     * 
+     * @return
+     */
+    public double confidenceHi() {
+    // high endpoint of 95% confidence interval
+	return confidenceHi;
+    }
+
+    public static void main(String[] args) {
+    // test client (described below)
+        int N = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        StdOut.println("input of grid size: " + N);
+        StdOut.println("input of trials number: " + trials);
+        PercolationStats P = new PercolationStats(N, trials);
+        StdOut.println("mean: " + P.mean());
+        StdOut.println("stddev: " + P.stddev());
+        StdOut.println("confidenceLo: " + P.confidenceLo());
+        StdOut.println("confidenceHi: " + P.confidenceHi());
+    }
 }
